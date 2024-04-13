7a275a9e003df83524495cf83ca4a48020c3a345
FaeelViegas <rafaeel410@gmail.com>
2024-04-13 19:45:35 -0300

    cria responsividade do menu de categorias

---
+++ js/header.js
@@ -0,0 +1,11 @@
+const menuButton1 = document.getElementById('menu-button-1');
+const menuButton2 = document.getElementById('menu-button-2');
+
+
+function toggleMenu(event) {
+  const menu = document.getElementById('container-menu');
+  menu.classList.toggle('active');
+}
+
+menuButton1.addEventListener('click', toggleMenu);
+menuButton2.addEventListener('click', toggleMenu);
