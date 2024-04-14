de52763f8b95aaf8c1b8bcbef699198168572bf9
FaeelViegas <rafaeel410@gmail.com>
2024-04-13 23:51:43 -0300

    correções de caminho de arquivos

--- js/home.js
+++ js/home.js
@@ -1,15 +1,3 @@
-const menuButton1 = document.getElementById('menu-button-1');
-const menuButton2 = document.getElementById('menu-button-2');
-
-
-function toggleMenu(event) {
-    const menu = document.getElementById('container-nav');
-    menu.classList.toggle('active');
-}
-
-menuButton1.addEventListener('click', toggleMenu);
-menuButton2.addEventListener('click', toggleMenu);
-
 const $simpleCarousel = document.querySelector(".js-carousel--simple");
 
 new Glider($simpleCarousel, {
